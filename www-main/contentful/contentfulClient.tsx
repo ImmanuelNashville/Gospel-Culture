e41db4a7f4a5912a665e1a7d17ddc446d3c29368
Matthew Brown <matt@platform-studios.com>
2025-02-06 06:45:04 -0500

    Fix Contentful env variable loading & refactor client setup

--- www-main/contentful/contentfulClient.tsx
+++ www-main/contentful/contentfulClient.tsx
@@ -1,14 +1,14 @@
 import { createClient } from 'contentful';
 
 const contentfulClient = createClient({
-  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID || '',
-  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_CONTENT_DELIVERY_API_ACCESS_TOKEN || '',
+  space: process.env.CONTENTFUL_SPACE_ID || '',
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY || '',
   removeUnresolved: true,
 });
 
 export const contentfulPreviewClient = createClient({
-  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_CONTENT_PREVIEW_API_ACCESS_TOKEN || '',
-  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID || '',
+  accessToken: process.env.CONTENTFUL_CONTENT_PREVIEW_API_ACCESS_TOKEN || '',
+  space: process.env.CONTENTFUL_SPACE_ID || '',
   host: 'preview.contentful.com',
 });
 
